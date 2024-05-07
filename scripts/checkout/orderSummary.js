b099ba50f6f96bfaf042982058e330aa9c025d14
7FOX7 <kheeugene@gmail.com>
2024-05-07 14:02:01 -0400

    Created a function 'calculateDeliveryDate(deliveryOption) in deliveryOptions.js to reuse it in 'orderSummary.js' file

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,11 +1,9 @@
 import {cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeliveryOption} from '../../data/cart.js';
 import {products, getProduct} from '../../data/products.js';
 import formatCurrency from '../utils/money.js'; 
-import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
-import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
+import {deliveryOptions, getDeliveryOption, calculateDeliveryDate} from '../../data/deliveryOptions.js';
 import {renderPaymentSummary} from './paymentSummary.js';
 import { renderCheckoutHeader } from './checkoutHeader.js';
-
 
 export function renderOrderSummary() {
 
@@ -15,19 +13,9 @@ export function renderOrderSummary() {
       const productId = cartItem.productId; 
 
       const matchingProduct = getProduct(productId); 
-
       const deliveryOptionId = cartItem.deliveryOptionId;
-
-      const deliveryOption = getDeliveryOption(deliveryOptionId); 
-      const today = dayjs();
-      const deliveryDate = today.add(
-        deliveryOption.deliveryDays,
-        'days'
-      );
-
-      const dateString = deliveryDate.format(
-        'dddd, MMMM D'
-      );
+      const deliveryOption = getDeliveryOption(deliveryOptionId);
+      const dateString = calculateDeliveryDate(deliveryOption);
       
       cartSummaryHTML +=
       `
@@ -75,16 +63,9 @@ export function renderOrderSummary() {
 
   function deliveryOptionsHTML(matchingProduct, cartItem) {
     let html = ''; 
-    const today = dayjs();
+    // const today = dayjs();
     deliveryOptions.forEach((deliveryOption) => {
-      const deliveryDate = today.add(
-        deliveryOption.deliveryDays,
-        'days'
-      );
-
-      const dateString = deliveryDate.format(
-        'dddd, MMMM D'
-      );
+      const dateString = calculateDeliveryDate(deliveryOption);
       
       const priceString = deliveryOption.priceCents === 0 ? 'FREE ' : `$${formatCurrency(deliveryOption.priceCents)} - `; 
 
