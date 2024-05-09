3845383e9c4549ec52b1ff1eb432c01908e22428
7FOX7 <kheeugene@gmail.com>
2024-05-09 11:24:13 -0400

    Modified cartTest.js so, a new product was added to the cart

--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -31,10 +31,10 @@ describe('test suite: addToCart', () => {
 
         
         loadFromStorage(); 
-        addToCart('15b6fc6f-327a-4ec4-896f-486349e85a3d');
+        addToCart('cc2847e9-4444-403f-b7cf-57fde044a955');
         expect(cart.length).toEqual(1); 
         expect(localStorage.setItem).toHaveBeenCalledTimes(1); 
-        expect(cart[0].productId).toEqual('15b6fc6f-327a-4ec4-896f-486349e85a3d');
+        expect(cart[0].productId).toEqual('cc2847e9-4444-403f-b7cf-57fde044a955');
         expect(cart[0].quantity).toEqual(1); 
     });  
 });
