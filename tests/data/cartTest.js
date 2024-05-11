20b1c6b30cdfe49176d73e6f48b2cd0c4a03575d
7FOX7 <kheeugene@gmail.com>
2024-05-11 16:36:58 -0400

    Converted products into classes and made chages on test cases by adding a second parameter to the 'addToCart()' function in the cartTest.js

--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -17,11 +17,11 @@ describe('test suite: addToCart', () => {
 
          
         loadFromStorage(); 
-        addToCart('15b6fc6f-327a-4ec4-896f-486349e85a3d');
+        addToCart('15b6fc6f-327a-4ec4-896f-486349e85a3d', '3');
         expect(cart.length).toEqual(1); 
         expect(localStorage.setItem).toHaveBeenCalledTimes(1); 
         expect(cart[0].productId).toEqual('15b6fc6f-327a-4ec4-896f-486349e85a3d');
-        expect(cart[0].quantity).toEqual(2); 
+        expect(cart[0].quantity).toEqual(4); 
          // 16c: 
         expect(localStorage.setItem).toHaveBeenCalledWith('cart', `${JSON.stringify(cart)}`); 
     }); 
@@ -33,16 +33,16 @@ describe('test suite: addToCart', () => {
         });
 
         loadFromStorage(); 
-        expect(addToCart('cc2847e9-4444-403f-b7cf-57fde044a955'));
+        expect(addToCart('cc2847e9-4444-403f-b7cf-57fde044a955', '2'));
         expect(cart.length).toEqual(1); 
         expect(localStorage.setItem).toHaveBeenCalledTimes(1); 
         expect(cart[0].productId).toEqual('cc2847e9-4444-403f-b7cf-57fde044a955');
-        expect(cart[0].quantity).toEqual(1); 
+        expect(cart[0].quantity).toEqual(2); 
 
         // 16d:
         expect(localStorage.setItem).toHaveBeenCalledWith('cart', `${JSON.stringify([{
             productId: 'cc2847e9-4444-403f-b7cf-57fde044a955',
-            quantity: 1, 
+            quantity: 2, 
             deliveryOptionId: '1'
         }])}`); 
     });  
